3c31c2ef7a91e29d9daf1acb13e532c88d2fa4d0
Doron Segal <doron2402@gmail.com>
2014-02-19 22:56:57 -0800

    handle not auth page

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,7 +1,7 @@
 exports.checkAuth = function(req, res, next){
 	console.log('Checking Auth...');
 	if (!req.session.user_id || !req.session.userId)
-    	res.send('You are not authorized to view this page');
+    	return res.render('errors/not_auth', {layout: 'layouts/error'});
   	else 
     	next();
   
